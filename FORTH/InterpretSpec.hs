c30cfb85c74443fee24aaed567b3ce94cb0d6f44
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-03-01 03:03:49 -0500

    #issue10: implemented CONCAT2 and CONCAT3 function to concat 2 and 3 argument as string and added test cases to.

--- FORTH/InterpretSpec.hs
+++ FORTH/InterpretSpec.hs
@@ -31,11 +31,51 @@ main = hspec $ do
 
     context "EMIT" $ do
         it "prints character from integer" $ do
-            interpret "65 EMIT" `shouldBe` ([], "A")
+            interpret "65 EMIT" `shouldBe` ([], "65 : A")
         it "prints character from real" $ do
-            interpret "66.9 EMIT" `shouldBe` ([], "B")
+            interpret "66.9 EMIT" `shouldBe` ([], "66 : B")
         it "errors on non-numeric" $ do
             evaluate (interpret "X EMIT") `shouldThrow` errorCall "Non-numeric argument to EMIT"
+
+    context "CONCAT2" $ do
+      it "concatenates two strings in reverse order" $ do
+        interpret "Hello World CONCAT2" `shouldBe` ([], "HelloWorld")
+        interpret "PLP ! CONCAT2" `shouldBe` ([], "PLP!")
+
+      it "errors on non-string arguments" $ do
+        evaluate (interpret "5 3 CONCAT2") `shouldThrow` errorCall "Type Mismatch"
+        evaluate (interpret "3.14 \"x\" CONCAT2") `shouldThrow` errorCall "Type Mismatch"
+
+      it "errors on insufficient stack elements" $ do
+        evaluate (interpret "\"alone\" CONCAT2") `shouldThrow` errorCall "Stack underflow"
+        evaluate (interpret "CONCAT2") `shouldThrow` errorCall "Stack underflow"
+
+    context "CONCAT3" $ do
+          it "concatenates two strings in reverse order" $ do
+            interpret "Hello World ! CONCAT3" `shouldBe` ([], "HelloWorld!")
+            interpret "! PLP Love CONCAT3" `shouldBe` ([], "!PLPLove")
+
+          it "errors on non-string arguments" $ do
+            evaluate (interpret "5 3 4 CONCAT3") `shouldThrow` errorCall "Type Mismatch"
+            evaluate (interpret "3.14 x 5 CONCAT3") `shouldThrow` errorCall "Type Mismatch"
+
+          it "errors on insufficient stack elements" $ do
+            evaluate (interpret "alone CONCAT3") `shouldThrow` errorCall "Stack underflow"
+            evaluate (interpret "CONCAT3") `shouldThrow` errorCall "Stack underflow"
+
+    context "STR" $ do
+        it "converts integer to string" $ do
+            interpret "5 STR" `shouldBe` ([], "5")
+        it "converts real to string" $ do
+            interpret "3.14 STR" `shouldBe` ([], "3.14")
+        it "leaves Id (string) unchanged" $ do
+            interpret "hello STR" `shouldBe` ([], "\"hello\"")
+        it "errors on empty stack" $ do
+            evaluate (interpret "STR") `shouldThrow` errorCall "Stack underflow"
+
+    context "CR" $ do
+        it "prints newline" $ do
+            interpret "CR" `shouldBe` ([], "\n")
 
     context "Printout" $ do
         it "computes product and outputs" $ do
